04cf0619ede1762ed6e43d4c0ae85ecbfebc73d8
isahxs <isabelly.sfernandes@senacsp.edu.br>
2026-03-27 11:41:46 -0300

    07-classe-herança

--- 06-atividade/script.js
+++ 06-atividade/script.js
@@ -1,74 +1,209 @@
-//Exercício 1 - Classe (Psicólogo)
-
-/* Crie uma classe chamada Psicologo com:
-
--nome
--valorSessao
-
-Crie um método chamado calcularFaturamento(qtdSessoes) que receba a 
-quantidade de sessões e retorne o faturamento.
-
-Depois:
-
-1.Crie uma instância.
-2.Calcule o faturamento para 30 sessões.
-*/
-
-class Psicologo {
-    constructor(nome, valorSessao) {
-        this.nome = nome;
-        this.valorSessao = valorSessao;
+//Exercício 1 - Psicólogo
+ 
+function avaliarSessao (nomePaciente, quantidadeSessoes) {
+    console.log("Nome do paciente: " + nomePaciente);
+ 
+    if(quantidadeSessoes >= 10) {
+        return true;
+    } else {
+        return false;
     }
+ 
+}  
 
-    calcularFaturamento(qntSessoes){
-        return this.valorSessao * qntSessoes;
+let resultado = avaliarSessao("Isa", 20);
+console.log(resultado);
+ 
+
+//Exercício 2 - Bolo de Pote
+ 
+const calcularValorPedido = (quantidadeBolos, valorUnitario) => {
+    let total = quantidadeBolos * valorUnitario;
+ 
+    if(quantidadeBolos >= 10) {
+        total = total - (total * 0.10);
     }
+    return total;
 }
-
-const psicologo = new Psicologo('Isabelly Fernandes', 120.00);
-
-const faturamentoTotal = psicologo.calcularFaturamento(30);
-console.log(`O faturamento para 30 sessões é R$ ${faturamentoTotal.toFixed(2)}`);
-
-//Exercício 2 - Herança (Ateliê de Costura)
-/*Pergunta:
-
-Crie:
--Uma classe Servico com tipo e valor
--Um método descricao()
-
-Depois crie uma classe Costura que herda de Servico e tenha:
--prazoDias
-
-Crie um objeto e mostre todas as informações.
-*/
-
-class Servico {
-    constructor(tipo, valor){
-        this.tipo = tipo;
-        this.valor = valor;
-    }
-
-    descricao() {
-        return `Servico: ${this.tipo} - Valor: R$${this.valor}`;
-    }
+    let valor = calcularValorPedido(10, 12.00);
+    console.log(valor);
+ 
+ 
+//Exercício 3
+ 
+function listarMateriais(materiais) {
+    console.log("Total de materiais: " + materiais.length);
+ 
+ 
+    let materiaisMaiusculos = materiais.map(function(material){
+    return material.toUpperCase();
+    });
+ 
+    return materiaisMaiusculos;
 }
-
-class Costura extends Servico {
-    constructor(tipo, valor, prazoDias) {
-
-        super(tipo, valor);
-        this.prazoDias = prazoDias;
-    }
-
-    descricao() {
-        return `${super.descricao()} - Prazo: ${this.prazoDias} dias`;
-    }
+    let lista = ["tinta", "pincel", "tecido"];
+    let check = listarMateriais(lista);
+    console.log(check);
+ 
+ 
+ 
+//Exercício 4 - Confeitaria
+ 
+const filtrarSaboresDisponiveis  = (sabores, letra) => {
+    let saboresFiltrados = sabores.filter(sabor => sabor[0] === letra);
+ 
+    return saboresFiltrados;
 }
+ 
+let sabores = ["chocolate", "morango", "maracujá", "coco"];
+let filtragemSabores = filtrarSaboresDisponiveis(sabores, "m");
+console.log(filtragemSabores);
 
-const servicoCostura = new Costura("Barra de calça jeans", 40.00, 1);
-console.log(servicoCostura.descricao());
+
+//JSON - Exercícios
+
+const quebraLinha = "\n";
+
+// Desafio 01
+// Function
+
+// Desafio 02
+// Arrow Function
+
+// Desafio 03
+// Function
+
+// Desafio 04
+// Function
+
+// Desafio 05
+// JSON
+
+console.log("---Psicólogo---");
+console.log("Paciente:" + quebraLinha);
+
+const paciente = `{
+    "nome": "Graziele Alves",
+    "idade": 42,
+    "plano-saude": false,
+    "queixas": [
+        "Insônia",
+        "Irritabilidade"
+    ],
+    "ultima-sessao": {
+        "data": "2026-03-16",
+        "humor": 7,
+        "prescricao": "Exercício físico diário"
+    }
+}`;
+
+console.log(paciente + quebraLinha);
+
+// Desafio 06
+// JSON
+
+console.log("---Ateliê---");
+console.log("Pedido:" + quebraLinha);
+
+const pedido = `{
+    "dados-cliente": {
+        "nome": "John Santos",
+        "telefone": "(11) 97548-0382"
+    },
+    "tipo-peca": "Camisa",
+    "medidas": {
+        "busto": "12cm",
+        "cintura": "8cm",
+        "quadril": "4cm"
+    },
+    "tecido-escolhido": "Seda",
+    "prazo": "4 Dias",
+    "status": [
+        "Recebido",
+        "Em Produção",
+        "Pronto",
+        "Entregue"
+    ]
+}`;
+
+console.log(pedido + quebraLinha);
+
+// Desafio 07
+// JSON
+
+console.log("---Bolo Pote---");
+console.log("Catálogo:" + quebraLinha);
+
+const catalogo = `{
+    promocaoAtiva: true ,
+    descontoPercentual: 15,
+
+    "sabores" : [
+    {   
+        "nome: " + "Chocolate com Morango",
+        "preco: " + 12.00,
+        "contemLactose: " + true,
+        "ingredientes: " + ["chocolate", "morango", "leite condensado", "creme de leite"] 
+
+    };
+    {   
+        "nome: " + "Morango com Coco",
+        "preco:"  + 10.50,
+        "contemLactose: " + false,
+        "ingredientes: " + ["coco ralado", "morango", "leite condensado sem lactose", "creme de leite"] 
+    };
+    {   
+        "nome: " : "Doce de Leite com Chocolate",
+        "preco: " : 13.00,
+        "contemLactose: " : false,
+        "ingredientes: " : ["doce de leite", "chocolate", "leite condensado", "creme de leite"]
+    };
+    ]
+
+}`;
+
+console.log(catalogo + quebraLinha);
 
 
+// Desafio 08
+// JSON
 
+console.log("---Confeitaria---");
+console.log("Catálogo:" + quebraLinha);
 
+const pedidoOnline = `{
+    "id": 620010,
+    "data-pedido": "2026-03-18",
+    "cliente": {
+        "nome": "Thomas Santos",
+        "email": "thomas.ssouza@gmail.com"
+    },
+    "itens": [
+        {
+            "produto": "Bolo Formigueiro",
+            "quantidade": 1,
+            "observacoes": "Nenhuma"
+        },
+        {
+            "produto": "Brigadeiro e Beijinho",
+            "quantidade": 30,
+            "observacoes": "Nenhuma"
+        }
+    ],
+    "endereco-entrega": {
+        "cep": "04750-000",
+        "cidade": "São Paulo",
+        "bairro": "Santo Amaro",
+        "rua": "Dr. Antônio Bento",
+        "numero": 393
+    },
+    "forma-pagamento": [
+        "Cartão de Crédito",
+        "Cartão de Débito",
+        "Boleto Bancário",
+        "PIX"
+    ],
+    "total-calculado": 150
+}`;
+
+console.log(pedidoOnline);
